8f0f139b9dc13337e4cb98f9d7aaaf457dec8056
Shota <shota1wa@yahoo.co.jp>
2019-08-09 09:30:30 -0700

    Added Generative Layer

--- js/script.js
+++ js/script.js
@@ -42,11 +42,6 @@ var points = [];
 
 
 /************** Location Descriptions *******************/
-/* The array includes 
-    [0] = name
-    [1] = Description
-    [2] = Practices    
-*/
 
 // Stillness
 var paccarArray = ['<h1>Paccar Hall</h1>' +
@@ -90,26 +85,61 @@ var parnassusArray = ['<h1>Parnassus Art Gallery Cafe</h1>' +
 	'<li>Spend a minute looking around the room and noticing what things people are doing, or what mood they seem to be in. Pick a genre of music that fits the mood of the cafe and listen to a few songs in that genre.</li></ul>'
 ];
 
-// Stillness locations
+// Generative
+var herbGardenArray = ['<h1>Medicinal Herb Garden</h1>' +
+    '<p>The Medicinal Herb Garden, a vibrant 2-acre garden near Rainier Vista, was started in 1911 by the UW Department of Pharmacy. Inherited by the Botany* department in 1979, the garden retains a collection spanning 800 to 1,000 plant taxa today. The plants come from environments all around the world – dry grassland, tropical rainforest, mountain, and desert.  Reflecting their geographic diversity, the Medicinal Herb Garden plants are used in healing traditions across hundreds of cultures.</p>' +
+    '<h2>Practices</h2>' + 
+    '<ul><li>Empathetic Visualization: Imagine you are one of the objects or people around you.</li>' +
+    '<li>Scent-imental contemplation: Think of a smell associated with a positive memory, and try to focus on it.</li>' +
+    '<li>Stress Visualization: Visualize your stress as an abstract object, make it as simple or complex as you want.</li>' +
+    '<li>Memory Alchemy: Transform an object of personal significance into a new memory.</li></ul>'
+];
+var sylvanArray = ['<h1>Sylvan Grove</h1>' +
+    '<p>A low, stone stairway vanishing into the trees leads to the Sylvan Theater, the site of early-day graduation ceremonies. This tree-enclosed hideaway features a grassy stage set with the four white columns that once graced the front stoop of the original university building</p>' +
+    '<h2>Practices</h2>' +
+    '<ul><li>Meditation</li>' +
+    '<li>Scent-imental contemplation</li></ul>'
+];
+var dockArray = ['<h1>Conibear Shellhouse Dock</h1>' +
+    '<p>UW’s Conibear Shellhouse sits on the shores of Lake Washington and has served as the primary home for Washington Rowing since its construction in 1949.The shellhouse is located “on campus,” only a light 6-8 minute walk from Drumheller Fountain, at the center of the UW academic campus. Named for Hiram Conibear, the founding father of rowing at the UW, the shellhouse underwent a complete renovation in 2005 increasing space by 75 per cent to 47,250 square feet resulting in a unique, multi-purpose facility at the heart of the university’s athletic campus.</p>' +
+    '<h2>Practices</h2>' +
+    '<ul><li>Meditation</li>' +
+    '<li>Scent-imental contemplation</li></ul>'
+];
+var henryArray = ['<h1>Henry Art Gallery</h1>' +
+    '<h2>Practices</h2>' +
+    '<ul><li>Meditation</li>' +
+    '<li>Scent-imental contemplation</li></ul>'
+]
+
+// Stillness Markers
 var paccar = L.marker([47.6591, -122.3086]).bindPopup(paccarArray.toString());
 grieg = L.marker([47.6562, -122.3066]).bindPopup(griegArray.toString());
 sakuma = L.marker([47.651838, -122.314841]).bindPopup(sakumaArray.toString());
 horticulture = L.marker([47.658474, -122.289997]).bindPopup(horticultureArray.toString());
 
-// Creativity Locations 
+// Creativity Markers 
 var skyspace = L.marker([47.656312, -122.31180]).bindPopup(skyspaceArray.toString());
 ecc = L.marker([47.655312, -122.314805]).bindPopup(eccArray.toString());
 wallDeath = L.marker([47.655225, -122.318511]).bindPopup(wallDeathArray.toString());
 parnassus = L.marker([47.658391, -122.306556]).bindPopup(parnassusArray.toString());
 
+// Generative Markers
+var herbGarden = L.marker([47.652580, -122.307884]).bindPopup(herbGardenArray.toString()); 
+var sylvan = L.marker([47.652705, -122.306400]).bindPopup(sylvanArray.toString());
+var dock = L.marker([47.652742, -122.299895]).bindPopup(dockArray.toString());
+var henry = L.marker([47.656149, -122.311776]).bindPopup(henryArray.toString());
+
 // Layer groups
 var stillness = L.layerGroup([paccar, grieg, sakuma, horticulture]);
 var creativity = L.layerGroup([skyspace, ecc, wallDeath, parnassus]);
+var generative = L.layerGroup([herbGarden, sylvan, dock, henry]);
 
 // objects for the layers
 var overlayMaps = {
 	"Stillness": stillness,
-	"Creativity": creativity
+    "Creativity": creativity,
+    "Generative": generative
 };
 
 // Adds the layers to the map
